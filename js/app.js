75b5a68b6d624dbd6cf052e68a0e4541e7c50ab8
CaptMerica <rmerica@live.com>
2023-02-21 22:10:35 -0500

    create function

--- js/app.js
+++ js/app.js
@@ -15,8 +15,24 @@ let board, turn, winner, tie;
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById('message');
-const resetBtnEl = document.querySelector('button');
+const resetBtnEl = document.querySelector('#button');
 /*----------------------------- Event Listeners -----------------------------*/
-// document.querySelector('.board').addEventListener('click', handleClick)
-// resetBtnEl.addEventListener('click', init)
-/*-------------------------------- Functions --------------------------------*/ 
+document.querySelector('.board').addEventListener('click', handleClick);
+resetBtnEl.addEventListener('click', init);
+/*-------------------------------- Functions --------------------------------*/
+function handleBtnClick(evt) {
+    if (!outputEl?.textContent || !inputEl?.value)
+        return;
+    if (!evt.target || !('id' in evt.target))
+        return;
+    const currentOutputValue = parseInt(outputEl.textContent);
+    const inputValue = parseInt(inputEl.value);
+    if (isNaN(currentOutputValue) || isNaN(inputValue))
+        return;
+    if (evt.target.id === 'plus') {
+        outputEl.textContent = (currentOutputValue + inputValue).toString();
+    }
+    else {
+        outputEl.textContent = (currentOutputValue - inputValue).toString();
+    }
+}
